62228ebb6ead111c997f51d591221410f3b08c1c
ExogenesisBen <ben.d.jackson@outlook.com>
2016-02-21 10:06:49 +0000

    Added github repo getting

--- social-face-recog/src/isaacjordan/me/FaceRecog/GitHubFeed.java
+++ social-face-recog/src/isaacjordan/me/FaceRecog/GitHubFeed.java
@@ -1,33 +1,48 @@
 package isaacjordan.me.FaceRecog;
 
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.egit.github.core.client.GitHubClient;
+import org.kohsuke.github.GHUserSearchBuilder;
+import org.kohsuke.github.GitHub;
+
+import twitter4j.Twitter;
 
 
 public class GitHubFeed implements SocialFeed {
-	private static final long serialVersionUID = -767532415720538684L;
+	
+	GitHub github = null;
 	String username;
-	List<Post> latestPosts = null;
+	List<Post> latestPost;
 	
 	public GitHubFeed(String username) {
 		this.username = username;
+		latestPost = new ArrayList<Post>();	
 	}
-
+		
+	
 	@Override
 	public void updateLatestPosts() {
+		try {
+			GitHub github = GitHub.connect();
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+		GHUserSearchBuilder search = github.searchUsers();
+		GHUserSearchBuilder results = search.repos(username);
+		Post post = new Post();
+		post.summary = results.toString();
+		latestPost.add(post);	
 	}
-
 	@Override
 	public String getUsername() {
 		return username;
 	}
-
 	@Override
 	public List<Post> getLatestPosts() {
-		// TODO Auto-generated method stub
-		return null;
+		return latestPost;
 	}
-
+	
 }
